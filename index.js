69a506c4c406aca9cc871c9fa81b6adba3cb882a
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-15 23:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {
 return "I can\'t hear you!"
 }
-}
 if (string.toUpperCase() === string) {
 return "YES INDEED!"
 }
-if (string()='I love you grandma') {
+if (string ==="I love you, Grandma.") {
   return "I love you too"
 }
