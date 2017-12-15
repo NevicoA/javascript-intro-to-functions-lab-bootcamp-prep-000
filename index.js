d78f83cdce62f1a7476a226f7a7869091e48f0da
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-15 21:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function logWhisper(string) {
   console.log('hello'.toLowerCase())
   return string.toLowerCase()
 }
-function sayHiToGrandma(string) { 
+function sayHiToGrandma(string) {
 if (string.toLowerCase()===string) {
 return "I can't hear you!"
 }
-
